d148c5c1cd18758ec2ebab712a3b2b44ed9bed38
CasaTaloyum <casatwy@msn.com>
2014-10-10 21:55:28 +0800

    added some handy scripts

--- workers/builderWorker.js
+++ workers/builderWorker.js
@@ -1,12 +1,13 @@
 var Gearman = require("gearman").Gearman;
+var shell = require("execSync");
 
 var worker = new Gearman('localhost', 4730);
 
 worker.on("JOB_ASSIGN", function(job){
     console.log("job assigned");
     console.log(job);
-    result = "job completed";
-    worker.sendWorkComplete(job.handle, result);
+    result = shell.exec("pwd");
+    worker.sendWorkComplete(job.handle, result.stdout);
 });
 
 worker.on('NOOP', function(){
